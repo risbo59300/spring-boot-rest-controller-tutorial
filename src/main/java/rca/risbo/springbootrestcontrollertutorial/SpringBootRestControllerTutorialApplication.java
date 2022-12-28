92b0bbd37e9e98d424c2addb0b44bbe0148d37fb
GABA-MANO <ybgabamano@gmail.com>
2022-12-28 12:42:15 +0100

    Initial commit

---
+++ src/main/java/rca/risbo/springbootrestcontrollertutorial/SpringBootRestControllerTutorialApplication.java
@@ -0,0 +1,13 @@
+package rca.risbo.springbootrestcontrollertutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestControllerTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestControllerTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rca/risbo/springbootrestcontrollertutorial/SpringBootRestControllerTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package rca.risbo.springbootrestcontrollertutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestControllerTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
