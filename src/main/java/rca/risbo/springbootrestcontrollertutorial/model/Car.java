c0f4206ca3a6c8db84a9a4ad14dfa3225fcf961a
GABA-MANO <ybgabamano@gmail.com>
2022-12-28 22:46:11 +0100

    Ajout des validateur dans le model

--- src/main/java/rca/risbo/springbootrestcontrollertutorial/model/Car.java
+++ src/main/java/rca/risbo/springbootrestcontrollertutorial/model/Car.java
@@ -1,8 +1,11 @@
 package rca.risbo.springbootrestcontrollertutorial.model;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
 public class Car {
@@ -13,4 +16,43 @@ public class Car {
     private Integer horses;
     private Double price;
 
+    @NotNull(message = "Model must not be null")
+    @NotEmpty(message = "Model must have value")
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @NotNull(message = "Brand must not be null")
+    @NotEmpty(message = "Brand must have value")
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @NotNull(message = "Horses must not be null")
+    @Positive
+    public Integer getHorses() {
+        return horses;
+    }
+
+    public void setHorses(Integer horses) {
+        this.horses = horses;
+    }
+
+    @NotNull(message = "Price must not be null")
+    @Positive
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
